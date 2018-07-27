ff5baac65f256f59f63068d9268c382c2aa2c3ea
Jesse Sanderson <jessev93@live.ca>
2018-07-27 17:42:42 +0700

    First commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let = list=[
+    {friend:"Dana"},
+    {friend:"Karl"},
+    {friend:"Luke"},
+    {friend:"Bailey"},
+    {friend:"Taylor"}
+]
+
+for (f=0; f < 99; f++){
+    let person = list[f].friend; {
+        for (n = 99; n > 0; n--){
+            let = (minusOne = n -1)
+            if (n > 1){
+                console.log (n + " lines of code in the file, "  +  n  +  " lines of code, " + person + " strikes one out, clears it all out, "  +  minusOne  +  " lines of code in the file!");
+
+            }else {
+                console.log("1 line of code in the file, 1 line of code, " + person + " strikes one out, clears it all out, no more lines of code in the file!")
+            }
+        }
+    }
+}
